33bb2b14ca4f2364015015351d89f2e2f01d5008
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-15 16:04:38 -0300

    feat: Requisito 2 - Classe SimpleReport e seu método

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,23 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(produtos):
+        fabricacao_antiga = min([item["data_de_fabricacao"]for item in produtos])
+        validade_proxima = min([item["data_de_validade"]
+                               for item in produtos
+                               if datetime.strptime(item["data_de_validade"], "%Y-%m-%d") > datetime.now()])
+        empresa_frequente = SimpleReport.__find_most_common(produtos, "nome_da_empresa")
+
+        return f"""Data de fabricação mais antiga: {fabricacao_antiga}
+Data de validade mais próxima: {validade_proxima}
+Empresa com mais produtos: {empresa_frequente}"""
+
+    @staticmethod
+    def __find_most_common(lista, filtro):
+        empresas = [item[filtro] for item in lista]
+        mais_comum = Counter(empresas).most_common(1)[0]
+        return mais_comum[0]
+
