d68946882a0898a22ebc6bb3f606d097f047bdfe
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-15 15:09:11 -0300

    feat: Requisito 01 - Função test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    test_product = Product(
+        "01",
+        "Controle Remoto",
+        "CCI",
+        "22/04/2000",
+        "indeterminada",
+        "123456-ABC",
+        "Não molhar."
+    )
+
+    assert test_product.id == "01"
+    assert test_product.nome_do_produto == "Controle Remoto"
+    assert test_product.nome_da_empresa == "CCI"
+    assert test_product.data_de_fabricacao == "22/04/2000"
+    assert test_product.data_de_validade == "indeterminada"
+    assert test_product.numero_de_serie == "123456-ABC"
+    assert test_product.instrucoes_de_armazenamento == "Não molhar."
