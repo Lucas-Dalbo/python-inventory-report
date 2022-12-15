b015d0ad3ce83321d1b8fb1947e27fb917e4a943
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-15 16:48:58 -0300

    refactor: SimpleReport - reformulação das funções auxiliares

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,29 +2,27 @@ from datetime import datetime
 from collections import Counter
 
 
-def find_most_common(lista, filtro):
-    empresas = [item[filtro] for item in lista]
-    mais_comum = Counter(empresas).most_common(1)[0]
-    return mais_comum[0]
-
-
-def find_validade_proxima(produtos):
-    validades = []
-    for item in produtos:
-        validade = datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
-        if validade > datetime.now():
-            validades.append(item["data_de_validade"])
-    return min(validades)
-
-
 class SimpleReport:
-    @staticmethod
-    def generate(produtos):
+    @classmethod
+    def generate(self, produtos):
         fabricacao_antiga = min([item["data_de_fabricacao"]
                                 for item in produtos])
-        validade_proxima = find_validade_proxima(produtos)
-        empresa_frequente = find_most_common(produtos, "nome_da_empresa")
+        validade_proxima = self._find_validade_proxima(produtos)
+        empresa_frequente = self._find_most_common(produtos, "nome_da_empresa")
 
         return f"""Data de fabricação mais antiga: {fabricacao_antiga}
 Data de validade mais próxima: {validade_proxima}
 Empresa com mais produtos: {empresa_frequente}"""
+
+    def _find_most_common(lista, filtro):
+        empresas = [item[filtro] for item in lista]
+        mais_comum = Counter(empresas).most_common(1)[0]
+        return mais_comum[0]
+
+    def _find_validade_proxima(produtos):
+        validades = []
+        for item in produtos:
+            validade = datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
+            if validade > datetime.now():
+                validades.append(item["data_de_validade"])
+        return min(validades)
