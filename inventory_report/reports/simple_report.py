2fa22ef8d17ec1fc2284e434d3c2902ed2851c80
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-15 16:52:13 -0300

    refactor: SimpleReport - Método estático para encontrar a fabricação mais antiga

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,8 +5,7 @@ from collections import Counter
 class SimpleReport:
     @classmethod
     def generate(self, produtos):
-        fabricacao_antiga = min([item["data_de_fabricacao"]
-                                for item in produtos])
+        fabricacao_antiga = self._find_fabricacao_antiga(produtos)
         validade_proxima = self._find_validade_proxima(produtos)
         empresa_frequente = self._find_most_common(produtos, "nome_da_empresa")
 
@@ -14,11 +13,13 @@ class SimpleReport:
 Data de validade mais próxima: {validade_proxima}
 Empresa com mais produtos: {empresa_frequente}"""
 
+    @staticmethod
     def _find_most_common(lista, filtro):
         empresas = [item[filtro] for item in lista]
         mais_comum = Counter(empresas).most_common(1)[0]
         return mais_comum[0]
 
+    @staticmethod
     def _find_validade_proxima(produtos):
         validades = []
         for item in produtos:
@@ -26,3 +27,9 @@ Empresa com mais produtos: {empresa_frequente}"""
             if validade > datetime.now():
                 validades.append(item["data_de_validade"])
         return min(validades)
+
+    @staticmethod
+    def _find_fabricacao_antiga(produtos):
+        fabricaoes = [item["data_de_fabricacao"]
+                      for item in produtos]
+        return min(fabricaoes)
