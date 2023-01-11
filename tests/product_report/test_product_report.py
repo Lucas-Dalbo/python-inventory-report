8791315b7a8977b5a1ace8d5e120875e5afc51a2
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-11 14:00:26 -0300

    test: Relatorio do Produto Testa se uma instância de Product retorna a string esperada ao chamar o método __repr__

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    test_product = Product(
+        "01",
+        "Chocolate",
+        "Racta",
+        "22-04-2000",
+        "22-04-2001",
+        "123456-ABC",
+        "lugar fresco e arejado"
+    )
+
+    report = test_product.__repr__()
+
+    assert report == "O produto Chocolate fabricado em 22-04-2000 por Racta com validade até 22-04-2001 precisa ser armazenado lugar fresco e arejado."
