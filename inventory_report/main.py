3385a97b0b39663f3fe44663ba44883729f9b380
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-13 10:51:59 -0300

    feat: função main em inventory - Permite gerar relatorios através da linha de comando. - Para isso é preciso informar qual o arquivo e o tipo de relatorio, simples ou completo.

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    file_types = {
+        "xml": XmlImporter,
+        "csv": CsvImporter,
+        "son": JsonImporter
+    }
+
+    if sys.argv.__len__() != 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    unused, file, rep_type = sys.argv
+    file_type = file[-3:]
+    inventario = InventoryRefactor(file_types[file_type])
+    data = inventario.import_data(file, rep_type)
+
+    return print(data, end='')    
