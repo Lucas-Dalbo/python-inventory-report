af677592aebaa4849ef0498ef8b5145a62db1e16
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-13 09:33:46 -0300

    refactor: Classe InventoryRefactor - Preapara paa se tornar uma classe interavel com Iterable - Refatora load_data para import_data - Método import_data retorna um relatorio de tipo passado por parametro

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,12 +1,25 @@
+from collections.abc import Iterable
 from inventory_report.importer.importer import Importer
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
-class InventoryRefactor():
+class InventoryRefactor(Iterable):
     def __init__(self, importer: Importer):
         self.importer = importer
         self.data = []
+        self.__report_types = {
+            "simples": SimpleReport,
+            "completo": CompleteReport
+        }
 
-    def load_data(self, arquivo: str):
+    def import_data(self, arquivo: str, rep_type: str):
         data = self.importer.import_data(arquivo)
         for item in data:
             self.data.append(item)
+        
+        return self.__report_types[rep_type].generate(data)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
