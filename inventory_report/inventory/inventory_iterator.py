ed00b701dadf641aa37fc4a3206f48c9be988e80
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-13 09:36:30 -0300

    feat: classe InventoryIterator - Classe Iterator para InventoryRefactor

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,16 @@
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __next__(self):
+        item = self.data[self.index]
+
+        if not item:
+            raise StopIteration()
+        
+        self.index += 1
+        return item
